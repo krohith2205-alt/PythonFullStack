6d51c21ceeba107b467339ab3b75ddb9201000a9
Kalahasthi Rohith <krohith2205@gmail.com>
2026-06-22 13:07:45 +0530

    Add files via upload
    Day-1 python Introduction Class

---
+++ Python Introduction Class.py
@@ -0,0 +1,92 @@
+Python 3.13.13 (tags/v3.13.13:01104ce, Apr  7 2026, 19:25:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> '''
+... procedural language
+... -----------
+... --> this follows a step-by-step approach, where code is structure into procedures such as function or routines...
+... eg--> C
+... 
+... OOP'S
+... -----
+... --> this is based on concepts of object and class...
+... PYTHON
+... ------
+... 1. Dynamically typed
+... -------
+... --> Python know the type of data wea are passing to the variable...
+... 
+... 2. Interperter
+... ----------
+... --> Python excute code line-bi-line, if any error it will stop the execution in the and before will gives the output... 
+... --> 
+... eg
+... num= 89
+... print(type(num))
+... num_2= 67
+... total_= num+ num_2
+... print(id num)
+... print(id(num_2))
+... print(id(total_))
+... 
+... 3. High Level
+... 
+... int num=89
+... 
+... 1991--> Guide Russum
+... 
+... why uisng python
+... -----------
+... --> Easy Syntax
+... --> Cross- Platform
+... --> Wide Applications
+... --> Huge Library support 
+... 
+... Tokens
+... ----
+... 1. Keywords
+... -----
+... ---> There are reserved word in python
+... Eg ---> if, else, for, while
+... 
+... 2. Identifires
+... ---------
+... ---> Name given to variables, function, class
+
+3. Literals
+----------
+---> 89,"hello",4.56
+
+4. Operators
+----------
+---> +,-,*,/
+
+5. Punctuators
+---------
+(),{},[]
+
+Rules of Variable
+----------
+A-Z, a-z
+eg
+---
+num_1=7
+num=8
+num_2=9
+
+Special Char, number, space
+$num= 0
+su@m= 8
+s o = 89
+2num = 9
+
+Comments
+----------
+1. Single line ---> #
+2. Multi line ---> ''' ''', """ """
+
+num = 9
+if num % 2 ==0: #checking a number is even or odd
+print("even")
+else:
+print("even")
+'''
